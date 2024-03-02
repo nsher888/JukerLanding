e7862b33e7d73942df1f7994bd41f684e9ca970c
nsher888 <nikoshervashidze@gmail.com>
2024-03-03 02:07:02 +0400

    refactor

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -14,6 +14,7 @@ const LanguageSwitcher = () => {
 		<div className="flex space-x-2">
 			{currentLanguage !== "en" && (
 				<button
+					title="Change Language to English"
 					onClick={() => changeLanguage("en")}
 					className="px-4 py-2 bg-pink-600 rounded-md text-white font-semibold focus:outline-none hover:bg-pink-700">
 					<EnIcon />
@@ -21,6 +22,7 @@ const LanguageSwitcher = () => {
 			)}
 			{currentLanguage !== "ka" && (
 				<button
+					title="Change Language to Georgian"
 					onClick={() => changeLanguage("ka")}
 					className="px-4 py-2 bg-pink-600 rounded-md text-white font-semibold focus:outline-none hover:bg-pink-700">
 					<KaIcon />
