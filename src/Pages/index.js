f9ad2a59c63ca31f0e89522f256f04b9d61bb9e6
tp322d <107504056+tp322d@users.noreply.github.com>
2024-03-24 14:26:43 +0400

    Update index.js

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from "./Home.jsx";
 export { default as Contact } from "./Contact.jsx";
-export { default as Started } from "./GetStarted.jsx";
+export { default as GetStarted } from "./GetStarted.jsx";
