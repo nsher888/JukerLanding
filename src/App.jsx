1bc1ebce24190b50184eed392e33ce9091394d84
tp322d <107504056+tp322d@users.noreply.github.com>
2024-03-24 13:57:03 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import { Contact, Home } from "./Pages";
+import { Contact, Home, GetStarted } from "./Pages"; // Assuming Pages directory
 import { AnimatePresence } from "framer-motion";
 
 function App() {
-	const location = useLocation();
+  const location = useLocation();
 
-	return (
-		<AnimatePresence mode="wait">
-			<Routes location={location} key={location.pathname}>
-				<Route path="" element={<Home title="Juker - Home Page" />} />
-				<Route
-					path="contact"
-					element={<Contact title="Juker - Contact Page" />}
-				/>
-			</Routes>
-		</AnimatePresence>
-	);
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="" element={<Home title="Juker - Home Page" />} />
+        <Route path="contact" element={<Contact title="Juker - Contact Page" />} />
+        <Route path="get-started" element={<GetStarted title="Juker - Get Started" />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
 export default App;
